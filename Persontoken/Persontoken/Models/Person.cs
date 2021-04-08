daae3a8ac7cbb7a69f931117a1fffeb74de1dda5
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-08 19:35:05 -0400

    creando model Person

---
+++ Persontoken/Persontoken/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Persontoken.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PerosnId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+
+    }
+}
