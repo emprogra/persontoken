4c6bd3d1ed9284911bf629fb66694f890c490acc
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-10 11:20:18 -0400

    Se agrego el controlador mvc people

---
+++ Persontoken/WebPersontoken/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebPersontoken.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebPersontoken.Models.Person> People { get; set; }
+    }
+}
