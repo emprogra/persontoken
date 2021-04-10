98a3a4379c7b8d306a4f8c847c3f13df880df3af
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-10 11:13:13 -0400

    Se agrego el proyecto mvctoken

---
+++ Persontoken/WebPersontoken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPersontoken.Startup))]
+namespace WebPersontoken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
